e98ae7d32cbe4302f66667230bb8eb257c6e8711
enyge637 <enyge637@gmail.com>
2021-06-09 10:18:26 +0900

    react-hook useAsync

--- src/User.js
+++ src/User.js
@@ -1,9 +1,8 @@
-import userEvent from "@testing-library/user-event";
 import axios from "axios";
 import React from "react";
-import useAsync from "./useAsync";
+import { useAsync } from "react-async";
 
-async function getUser(id) {
+async function getUser({ id }) {
   const response = await axios.get(
     `https://jsonplaceholder.typicode.com/users/${id}`
   );
@@ -11,12 +10,20 @@ async function getUser(id) {
 }
 
 function User({ id }) {
-  const [state] = useAsync(() => getUser(id), [id]);
-  const { loading, data: user, error } = state;
+  const {
+    data: user,
+    error,
+    isLoading,
+  } = useAsync({
+    promiseFn: getUser,
+    id,
+    watch: id,
+  });
 
-  if (loading) return <div>로딩중...</div>;
+  if (isLoading) return <div>로딩중...</div>;
   if (error) return <div>에러가 발생</div>;
   if (!user) return null;
+
   return (
     <div>
       <h2>{user.username}</h2>
